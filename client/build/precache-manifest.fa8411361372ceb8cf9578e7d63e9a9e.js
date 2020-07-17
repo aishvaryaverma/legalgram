fbb1ea6c2da46a8a1a59c3c52c15038d7dc831ce
Pardeep Kumar <pardeep.kumar02@nagarro.com>
2020-07-17 18:31:36 +0530

    testing

---
+++ client/build/precache-manifest.fa8411361372ceb8cf9578e7d63e9a9e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c923b801706ac3c7f821155d3b1562ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae0b95a8ceae829f60e8",
+    "url": "/static/css/main.1a4485b4.chunk.css"
+  },
+  {
+    "revision": "e9d5207b26445527de0c",
+    "url": "/static/js/2.fdf117ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fdf117ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0b95a8ceae829f60e8",
+    "url": "/static/js/main.8ca21a8e.chunk.js"
+  },
+  {
+    "revision": "9759d56e108650cfe12b",
+    "url": "/static/js/runtime-main.d3435342.js"
+  },
+  {
+    "revision": "b21cb72af0812eee39f48fe449ce78af",
+    "url": "/static/media/first-aritcle.b21cb72a.jpg"
+  },
+  {
+    "revision": "2a2b25576bbe1199b9acbeba67ccf3e8",
+    "url": "/static/media/img-td-1.2a2b2557.jpg"
+  },
+  {
+    "revision": "7f5145c926ec6d25224fbd71c16b1f7c",
+    "url": "/static/media/img-td-2.7f5145c9.jpg"
+  },
+  {
+    "revision": "85800798790e5d67da5ef6682c59218f",
+    "url": "/static/media/img-td-3.85800798.jpg"
+  },
+  {
+    "revision": "7cb7fa6b891492c9c0ea5bd7d8d807f3",
+    "url": "/static/media/img-td-4.7cb7fa6b.jpg"
+  },
+  {
+    "revision": "ea5938607c2121e5885a4157ac4f8e77",
+    "url": "/static/media/logo.ea593860.png"
+  },
+  {
+    "revision": "85f074fea019a6e2ff070264c5b64c4c",
+    "url": "/static/media/mission.85f074fe.jpg"
+  },
+  {
+    "revision": "74ecd3315ba34ee6acf3cec50a1d0752",
+    "url": "/static/media/objective.74ecd331.jpg"
+  },
+  {
+    "revision": "4545fd433463d86ead9415879394a15a",
+    "url": "/static/media/values.4545fd43.jpg"
+  },
+  {
+    "revision": "5858e2aa748bda0e5255fb0bcf9f23c2",
+    "url": "/static/media/vision.5858e2aa.jpg"
+  }
+]);
