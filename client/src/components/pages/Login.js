10002d9fdfafb0e8c80030d2434cf80e5b6c4e3b
aishvaryaverma <vsonu9020@gmail.com>
2020-08-06 11:35:56 +0530

    auth routes updates

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../layout/Layout';
+import LoginPage from '../partials/auth/LoginPage';
 
 const Login = () => {
 	return (
 		<Layout>
-			Login
+			<LoginPage />
 		</Layout>
 	)
 }
---
+++ client/src/components/partials/auth/LoginPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LoginPage = () => {
+	return (
+		<section className="section">
+			<div className="container">
+				<div className="loginRegisterBox">
+					<div className="loginRegisterBox__content">
+						<h1 className="size50">Login to Your Account</h1>
+						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic ex libero sint, earum dolorum veniam voluptas magnam explicabo deserunt veritatis!</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default LoginPage
